dcf8e20165328680f3b3b2dbd81023c65bffdd87
siva <sivakumar091020035@gmail.com>
2026-04-30 11:13:18 +0530

    added dagshub

--- src/model/model_eval.py
+++ src/model/model_eval.py
@@ -3,16 +3,22 @@ import pandas as pd
 import pickle
 import json
 import os
+import mlflow
+import dagshub
+
+
+dagshub.init(repo_owner='sivakumar026', repo_name='water_quality_mlpipeline', mlflow=True)
+
 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # load test data
 test_data = pd.read_csv("./data/processed/test_processed.csv")
 
-X_test = test_data.iloc[:, 0:-1].values
+X_test = test_data.iloc[:, :-1].values
 y_test = test_data.iloc[:, -1].values
 
-# load model (FIX PATH)
+# load model
 model = pickle.load(open("models/model.pkl", "rb"))
 
 # predictions
@@ -24,6 +30,24 @@ pre = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1score = f1_score(y_test, y_pred)
 
+# ---------------- IMPORTANT FIX ----------------
+mlflow.set_tracking_uri("https://dagshub.com/sivakumar026/water_quality_mlpipeline.mlflow")
+mlflow.set_experiment("water_quality_pipeline")
+
+# ---------------- MLflow logging ----------------
+with mlflow.start_run() as run:
+
+    print("RUN ID:", run.info.run_id)
+
+    mlflow.log_metric("accuracy", float(acc))
+    mlflow.log_metric("precision", float(pre))
+    mlflow.log_metric("recall", float(recall))
+    mlflow.log_metric("f1_score", float(f1score))
+
+    mlflow.log_param("model_type", type(model).__name__)
+    mlflow.log_param("n_estimators", model.n_estimators)
+
+# ---------------- DVC metrics ----------------
 metrics_dict = {
     "accuracy": float(acc),
     "precision": float(pre),
@@ -33,7 +57,6 @@ metrics_dict = {
 
 os.makedirs("reports", exist_ok=True)
 
-# save in correct location for DVC
 with open("reports/metrics.json", "w") as file:
     json.dump(metrics_dict, file, indent=4)
 
